d0cef5833fd6298f2e6504679433e91b22c31df6
shaukat-aali <shaukat.aali.dev@gmail.com>
2026-01-19 20:57:57 +0530

    Add files via upload

---
+++ self learn d1.py
@@ -0,0 +1,32 @@
+#This is my first Python Program 
+print("I like izza")
+
+#Variable (string,interger,float,boolean)
+first_name= "Shaukat"
+food= "pizza"
+email= "shaukat123aali@gmail.com"
+print(f"Hello {first_name}")
+print(f"Your email is {email}")
+
+#Integers 
+
+age=23
+quantity= 3
+num_of_students=50
+print(f"Your age ={age}")
+print(f'Your class have {num_of_students}')
+
+#float
+
+price=10.99
+print(f"The price is {price}")
+
+#boolean
+
+Shaukat= False
+grate= True
+if Shaukat:
+    print("He is Greate")
+else:
+    print("He is not ")
+print("" )

99996634324ef7a13901e731b7686d9341e100e5
shaukat-aali <shaukat.aali.dev@gmail.com>
2026-01-21 16:56:58 +0530

    Add files via upload

--- self learn d4.py
+++ self learn d4.py
@@ -1,5 +1,39 @@
 #arthmatic operation
+#string +=1 (called augumented assignment operator)
+#friends=0
+#friends= friends + 1
+#friends +=4
+#friends -=2
+#friends *=3
+#friends/=2
+#friends **=2
+#reminder = friends % 3
+#print(reminder) 
 
-friends=0
-friends= friends + 1
-print(friends)
+x= 3.14
+y=-4
+z= 5
+#round= round(x)
+#result=abs(y)
+#result= pow(2,z)
+#result= max(x,y,z)
+#result=min(x,y,z)
+
+import math
+#print(math.pi)
+#print(math.e)
+#result = math.sqrt(x)
+#result = math.ceil(x)
+#result = math.floor(x)
+#print(result)
+      #AREA AND CIRCUMFERENCE FOR CIRCLE
+#radius= float  (input("Enter radius for Circle: "))
+#area= math.pi * radius * radius
+#Circum= 2* math.pi * radius
+#print(f"The circumference is :{round(Circum)}cm and the area is : {round(area)}cm")
+
+    #HYPOTANUS FOR TRIANGLE
+base = float (input("Enter the Base of the triangle:"))
+height= float (input("Enter the Height of the triangle:"))
+hypo= math.sqrt(pow(base ,2) + height)
+print(f"The Hypotenuse is :{hypo}") 

f478fb9b6f7ae573f64633c38a5e1acdf0ad5cf2
shaukat-aali <shaukat.aali.dev@gmail.com>
2026-01-22 17:55:55 +0530

    Add files via upload

---
+++ #Conditional expression.py
@@ -0,0 +1,18 @@
+#conditional expression:-if-else statement shortcut(ternary operator)
+#print or assign one of two values vbased on a condition
+#x if condition else Y.
+
+num=-24
+a=44
+b=7
+age=13
+temp=4
+#print("Positive" if num>0 else "Negetive")
+#result="EVEN" if num%2 == 0 else "ODD"
+
+#max=a if a>b else b
+#min=a if a<b else b
+#print(max , min) 
+status="Adult" if age>=18 else "Teenage"
+weather="HOT" if temp>35 else "Normal"
+print(weather)
